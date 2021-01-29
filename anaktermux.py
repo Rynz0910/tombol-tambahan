4975197968fcf0713e87dfd1e24bd05f73f5fd47
Rynz0910 <77886060+Rynz0910@users.noreply.github.com>
2021-01-29 08:53:22 +0700

    Add files via upload

---
+++ anaktermux.py
@@ -0,0 +1,16 @@
+import os
+from time import sleep
+
+
+a ='\033[92m'
+b ='\033[91m'
+c ='\033[0m'
+
+def setup():
+    try:
+        os.mkdir('/data/data/com.termux/files/home/.termux')
+    except:
+        pass
+    key = "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]"
+    open('/data/data/com.termux/files/home/.termux/termux.properties','w').write(key)
+    os.system('termux-reload-settings')
